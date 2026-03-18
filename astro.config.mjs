f063ced66c1b286aa2cc42d4a6e0349c5bda8f35
aman mohammed <aman.mohammed979@gmail.com>
2026-03-19 05:21:33 +0530

    Initial commit (reset repo)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://amanm.space",
@@ -11,4 +12,5 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+  adapter: cloudflare(),
 });
